95adffc5e3f122f1d958dcb4d2c30b37514a2c14
daoudi el hassan <elha_da@hotmail.com>
2018-01-11 17:14:40 +0100

    sample immutable RoundRobin akka

---
+++ src/main/scala/roundRobin/ImmutableRoundRobin.scala
@@ -0,0 +1,24 @@
+package roundRobin
+
+import akka.typed.scaladsl.Actor
+import akka.typed.{ActorRef, Behavior}
+
+object ImmutableRoundRobin {
+
+  def roundRobinBehavior[T](numberOfWorkers: Int, worker: Behavior[T]): Behavior[T] =
+    Actor.deferred { ctx =>
+      val workers = (1 to numberOfWorkers).map { n =>
+        println(s"roundRobinBehavior $n")
+        ctx.spawn(worker, s"worker-$n")
+      }
+      activeRoutingBehavior(index = 0, workers.toVector)
+    }
+
+  private def activeRoutingBehavior[T](index: Long, workers: Vector[ActorRef[T]]): Behavior[T] =
+    Actor.immutable[T] { (ctx, msg) =>
+      println(s"activeRoutingBehavior $index")
+
+      workers((index % workers.size).toInt) ! msg
+      activeRoutingBehavior(index + 1, workers)
+    }
+}
