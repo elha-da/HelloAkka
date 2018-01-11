62f18a5013ecbc2c29a762e5a8ad24defad5dfe6
daoudi el hassan <elha_da@hotmail.com>
2018-01-11 16:50:28 +0100

    add a sample test

---
+++ src/test/scala/MySpec.scala
@@ -0,0 +1,61 @@
+package firstSample
+
+import akka.actor.{ActorRef, ActorSystem, Props}
+import akka.testkit.{ImplicitSender, TestActors, TestKit, TestProbe}
+import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpecLike}
+
+import com.familyDependence.Family._
+
+import scala.concurrent.duration._
+
+
+class MySpec() extends TestKit(ActorSystem("MySpec"))
+                with ImplicitSender
+                with WordSpecLike
+                with Matchers
+                with BeforeAndAfterAll {
+
+  override def afterAll {
+    TestKit.shutdownActorSystem(system)
+  }
+
+  val printer: ActorRef = system.actorOf(TestActors.echoActorProps, "printer")
+  val workerForwarder: ActorRef = system.actorOf(TestActors.forwardActorProps(printer))
+
+  "An printer actor" must {
+    "send back messages unchanged by forworder actor" in {
+      within(200 millis) {
+        workerForwarder ! "some work"
+        expectMsg("some work")
+        expectNoMsg // will block for the rest of the 200ms
+        Thread.sleep(300) // will NOT make this block fail
+      }
+    }
+  }
+
+  "An echo actor" must {
+    "send back messages unchanged" in {
+//      val printer: ActorRef = system.actorOf(TestActors.echoActorProps, "echo-hello")
+      printer ! "hello world"
+      expectMsg(100 millis, "hello world")
+    }
+  }
+
+  "A TestProbe serving as parent" should {
+    "test its child responses" in {
+      val parent = TestProbe()
+      val child = parent.childActorOf(Props[Child](), "child")
+      parent.send(child, "ping")
+      parent.expectMsg("pong")
+    }
+  }
+
+  "A TestProbe serving as parent Actor" should {
+      "test its child responses 2" in {
+        val parent = system.actorOf(Props[Parent](), "child2")
+        parent ! "pingit"
+        expectMsg(5.second,"ping")
+      }
+    }
+
+}
